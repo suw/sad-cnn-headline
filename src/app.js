6b61413defd0e7d68f2824649b92636efac0a629
Su Wang <suw.mail@gmail.com>
2013-12-22 15:22:28 -0800

    Add check for Mexico edition of CNN

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+// Find content container
 var contentDiv = document.getElementById("cnnContentContainer");
+
+// Mexico edition uses a different DOM for content
+if (!contentDiv) {
+    contentDiv = document.getElementById("contenido");
+}
+
+// Add extra sadness
 var topHeader = contentDiv.getElementsByTagName("h1");
 topHeader[0].innerText = topHeader[0].innerText.concat(" :\/");
