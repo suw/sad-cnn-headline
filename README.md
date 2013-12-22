67127c7e2678e6596c91e0057a0d150e5bab338d
Su Wang <suw.mail@gmail.com>
2013-12-22 14:53:29 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sad CNN Headline
+
+Add a :\ face to every CNN story headline.. because they're all sad.
---
+++ src/app.js
@@ -0,0 +1,3 @@
+var contentDiv = document.getElementById("cnnContentContainer");
+var topHeader = contentDiv.getElementsByTagName("h1");
+topHeader[0].innerText = topHeader[0].innerText.concat(" :\/");
