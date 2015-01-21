74fb194e154512d0dd6afc89ffeb5196b780e073
Su Wang <suw.mail@gmail.com>
2015-01-20 20:08:06 -0800

    Fix issues with new style CNN

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
-// Find content container
+/*
+ * This is the old style CNN site, first find the content container
+ * then find the headline element
+ */
 var contentDiv = document.getElementById("cnnContentContainer");
 
 // Find the content div for Mexico edition
@@ -11,10 +14,15 @@ if (!contentDiv) {
     contentDiv = document.getElementById("storycontent");
 }
 
-// Add extra sadness
-var topHeader = contentDiv.getElementsByTagName("h1");
-topHeader[0].innerText = topHeader[0].innerText.concat(" :\/");
+var headlineElement;
 
-//For new US version
-var headlineDiv = document.getElementsByClassName("pg-headline");
-headlineDiv[0].innerText = headlineDiv[0].innerText.concat(" :\/");
+if (contentDiv) {
+    headlineElement = contentDiv.getElementsByTagName("h1");
+} else {
+    // The new style CNN site
+    headlineElement = document.getElementsByClassName("pg-headline");
+}
+
+// Add extra sadness
+headlineElement[0].innerText = headlineElement[0].innerText.concat(" :\/");
+
